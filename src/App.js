c3c0014a1947d463a89a466bad94dc8aa0f5744f
Richa Modi <123286601+RichiErichhH@users.noreply.github.com>
2024-01-17 19:00:56 +0530

    completing form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback} from 'react';
+import { useFormik} from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  firstName:yup.string().min(3).required(),
+  lastName:yup.string().min(3).required(),
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const handleOnSubmit =(values)=>{
+    const fullName = Object.keys(values)
+      .map((key) => values[key])
+      .join(" ");
+    alert(`Hello ${fullName}!`);
+  }
+  const formik = useFormik({
+    initialValues: {
+      firstName:"",
+      lastName:"",
+    },
+    validationSchema: schema,
+    onSubmit: handleOnSubmit,
+  })
+  
+  const setInputValue = useCallback(
+    (key, value) =>
+      formik.setValues({
+        ...formik.values,
+        [key]: value,
+      }),
+    [formik]
   );
+  console.log(formik)
+  
+  return(
+    <form onSubmit={formik.handleSubmit}>
+      <input type="text" placeholder="Enter your name" value={formik.values.firstName}
+        onChange={(e) => setInputValue("firstName", e.target.value)} name='First' />
+        <br/>
+        <small>{formik.errors.firstName}</small>
+        <br/>
+      <input type="text" placeholder="Enter your Last name" value={formik.values.lastName}
+        onChange={(e) => setInputValue("lastName", e.target.value)}  name='Lastname'/>
+        <br/>
+        <small>{formik.errors.lastName}</small>
+        <br/>
+        {!!formik.errors.lastName && <br />}
+      <button type="submit" disabled={!formik.isValid}>Submit</button>
+    </form>
+  )
 }
-
 export default App;
