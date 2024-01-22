596b2015d8fdff93047407770512415d8f201fc4
Richa Modi <123286601+RichiErichhH@users.noreply.github.com>
2024-01-22 19:07:55 +0530

    Form completed using formik and firebase

--- src/Signin.js
+++ src/Signin.js
@@ -1,57 +1,77 @@
 import React from 'react';
 import { Validation } from './Validation';
 import { useFormik } from 'formik';
-import { auth }from './Firbaseconfig';
-import  * as yup from "yup";
-import { signInWithEmailAndPassword,signOut } from 'firebase/auth';
+import { auth } from './Firbaseconfig';
+import * as yup from 'yup';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import img7 from './image/img7.jpg';
+import {  ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const initialValues = {
-    email : "",
-    password : "",
-}
+  email: '',
+  password: '',
+};
 
+export default function Signin() {
+    const notify = () => {toast("SignIn Successfully");}
 
+  const navigate = useNavigate();
 
-export default function Signin(){
-  const nevigate= useNavigate();
-    const{ handleSubmit , values, handleChange , handleBlur , errors , touched} =useFormik({
-        initialValues : initialValues,
-        validationSchema: yup.object(Validation),
-        onSubmit :  async (values, action) => {
-            try {
-            //    console.log("before sign in");
-              await signInWithEmailAndPassword(auth, values.email, values.password);
-            //   alert("successfully signed in");
-              console.log(values.email);
-              
-              
-            } catch (error) {
-              console.error('Sign-in error:', error.message);
-            }
-            nevigate("./home");
-            action.resetForm();
-          },
-    })
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label for="email">Email:</label>
-                <br/>
-                <input type="email" name="email" onBlur={handleBlur} onChange={handleChange} value={values.email}/>
-                <br/>
-                {touched.email && errors.email && <div>{errors.email}</div>}
-                <br/>
+  const { handleSubmit, values, handleChange, handleBlur, errors, touched } = useFormik({
+    initialValues: initialValues,
+    validationSchema: yup.object(Validation),
+    onSubmit: async (values, action) => {
+      try {
+        await signInWithEmailAndPassword(auth, values.email, values.password);
+        notify();
+        action.resetForm();
+        navigate('/signin/home'); 
+      } catch (error) {
+        console.error('Sign-in error:', error.message);
+        action.setErrors({ auth: error.message });
+      }
+    },
+  });
 
-                <label for="password">Password:</label>
-                <br/>
-                <input type='password' name="password" onBlur={handleBlur} onChange={handleChange} value={values.password}/>
-                <br/>
-                {touched.password && errors.password && <div>{errors.password}</div>}
-                <br/>
-                
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="left_signin">
+        <form onSubmit={handleSubmit}>
+          <div className="form_decoration">
+            <h1>SignIn Page</h1>
+            <label htmlFor="email">Email:</label>
+            <br />
+            <input type="email" name="email" onBlur={handleBlur} onChange={handleChange} value={values.email} />
+            <br />
+            {touched.email && errors.email && <div>{errors.email}</div>}
+            <br />
+
+            <label htmlFor="password">Password:</label>
+            <br />
+            <input type="password" name="password" onBlur={handleBlur} onChange={handleChange} value={values.password} />
+            <br />
+            {touched.password && errors.password && <div>{errors.password}</div>}
+            <br />
+            {errors.auth && <div>Incorrect Email or Password</div>}
+            <button type="submit" className="done">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="right_signin">
+        <img src={img7} alt="Right Signin" />
+      </div>
+      <ToastContainer 
+        position="top-center"
+        autoClose={3000}
+        pauseOnHover
+        draggable
+        closeOnClick
+        hideProgressBar={true}
+        />
+    </div>
+  );
 }
